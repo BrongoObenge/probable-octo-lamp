205cef537e8c4a080b7d5185a962b8223fd252f6
Jiar <jiarshengy@gmail.com>
2016-06-21 21:02:54 +0200

    forgot a class

---
+++ src/main/java/nl/hr/domain/MetaData.java
@@ -0,0 +1,14 @@
+package nl.hr.domain;
+
+import lombok.Value;
+
+/**
+ * Created by j on 6/21/2016.
+ */
+
+@Value
+public class MetaData {
+    double optimalAlpha;
+    double optimalGamma;
+    double SSE;
+}
